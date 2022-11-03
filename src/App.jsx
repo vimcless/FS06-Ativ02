df202dfbbe4493865e413f1fd174c1fe1f7ba4e2
vinicius <vinicius@vini21>
2022-11-03 12:15:18 -0300

    Adding Favorite folder and it index.jsx, changed files from .js to jsx and added favoritos folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar"
 import Carrinho from "./pages/Carrinho"
 import FormEndereco from "./pages/FormEndereco"
 import Products from "./pages/Products"
+import Favorite from "./pages/Favorite"
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
           <Route path="/listar" element={<Products/>} />
           <Route path="/endereco" element={<FormEndereco/>} />
           <Route path="/carrinho" element={<Carrinho/>} />
+          <Route path="/favoritos" element={<Favorite/>} />
         </Routes>
       </BrowserRouter>
     </div>
